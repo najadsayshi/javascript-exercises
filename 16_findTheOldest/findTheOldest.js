b8cf6c90cfb386e3eee12ddcb2f77f34376a9e41
Najad <mrnajadas@gmail.com>
2025-12-22 17:25:38 +0530

    done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  return people.reduce((oldest, person) => {
+    const oldestAge =
+      (oldest.yearOfDeath || new Date().getFullYear()) - oldest.yearOfBirth;
 
+    const personAge =
+      (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth;
+
+    return personAge > oldestAge ? person : oldest;
+  });
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
