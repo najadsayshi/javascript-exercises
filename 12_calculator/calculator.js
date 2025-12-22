2a029919bf1c7fe1437a47b6bd5a042039d31d08
Najad <mrnajadas@gmail.com>
2025-12-22 15:37:05 +0530

    Done Calculator.js

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,24 +1,37 @@
-const add = function() {
+const add = function(num1,num2) {
+  return num1+num2;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1,num2) {
+  return num1,-num2;
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  return array.reduce((accum,ele)=>  accum+ele);
 	
 };
 
 const multiply = function() {
+  return array.reduce((accum,ele)=>  accum*ele);
+
 
 };
 
-const power = function() {
+const power = function(num1,num2) {
+  return num1**num2;
 	
 };
 
-const factorial = function() {
+const factorial = function(num1) {
+  let i =0;
+  let current = 1;
+  while (i!=num1){
+    current *= i;
+    
+  }
+  return current;
 	
 };
 
