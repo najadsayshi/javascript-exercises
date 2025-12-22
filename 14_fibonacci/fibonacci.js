fda6c487dbe92370368d27e51a1ca5a119ca3f55
Najad <mrnajadas@gmail.com>
2025-12-22 16:43:57 +0530

    done fibonnaci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let first = 1;
+    let second = 2;
+    if (num==1){
+        return first;
+    }
+    else if (num==2){
+        return second;
+    }
 
-};
-
+    for (let i =0;i<num;i++){
+        output = first + second;
+        first = second;
+        second = output;
+        return output;
+    }
+}
+    
 // Do not edit below this line
 module.exports = fibonacci;
