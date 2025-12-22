40cbfc65d526d4f6001ed031349e8edf402445cf
Najad <mrnajadas@gmail.com>
2025-12-22 16:32:18 +0530

    with regex

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,6 @@
 const palindromes = function (string) {
     string = string.toLowerCase  ();
-    string.replace(' ','')
+    string.replace(/[^a-z0-9]/g,'')
     let l = 0
     let r = string.length-1;
     while (l<r){
